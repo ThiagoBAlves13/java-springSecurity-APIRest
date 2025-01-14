9fa45702eb6724e756565a217cfdbb206323adff
unknown <thiagobalves13@gmail.com>
2025-01-14 14:49:33 -0300

    feat(finalizada Aula01)[Aula_01]: finalizada aula 01 e concluido módulo 01 da aula 02

--- src/main/java/br/com/forum_hub/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/br/com/forum_hub/infra/security/ConfiguracoesSeguranca.java
@@ -2,16 +2,29 @@ package br.com.forum_hub.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class ConfiguracoesSeguranca {
 
-    @Bean
+        @Bean
+        public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception{
+                return http.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).csrf(csrf -> csrf.disable()).build();
+        }
+
+        @Bean
+        public PasswordEncoder encriptador(){
+                return new BCryptPasswordEncoder();
+        }
+
+        @Bean
         public WebSecurityCustomizer webSecurityCustomizer() {
                 return (web) -> web.ignoring().requestMatchers("/h2-console/**");
         }
