489d23d38f2007f311be3ba3847ea7c310cd3831
unknown <thiagobalves13@gmail.com>
2025-01-14 15:15:25 -0300

    feat(efetuarLogin)[Aula_02]: criado o método efetuarLogin

---
+++ src/main/java/br/com/forum_hub/controller/AutenticacaoController.java
@@ -0,0 +1,29 @@
+package br.com.forum_hub.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.forum_hub.domain.autenticacao.DadosLogin;
+import jakarta.validation.Valid;
+
+
+@RestController
+public class AutenticacaoController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> efetuarLogin(@RequestBody @Valid DadosLogin dados) {
+
+        var autenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+        return ResponseEntity.ok(authenticationManager.authenticate(autenticationToken));
+
+    }
+
+}
