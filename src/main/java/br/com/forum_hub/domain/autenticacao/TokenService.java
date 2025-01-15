b0183598fb9ecd21424fcdbee44906fd30bd77c0
unknown <thiagobalves13@gmail.com>
2025-01-15 15:43:49 -0300

    feat(valida toke acesso)[Aula_03]: Validando o Token de acesso

--- src/main/java/br/com/forum_hub/domain/autenticacao/TokenService.java
+++ src/main/java/br/com/forum_hub/domain/autenticacao/TokenService.java
@@ -3,13 +3,15 @@ package br.com.forum_hub.domain.autenticacao;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
 
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import br.com.forum_hub.domain.usuario.Usuario;
 import br.com.forum_hub.infra.exception.RegraDeNegocioException;
@@ -32,6 +34,21 @@ public class TokenService {
 
     private Instant expiracao(Integer minutos) {
         return LocalDateTime.now().plusMinutes(minutos).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String verificarToken(String token) {
+        DecodedJWT decodedJWT;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("12345678");
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("Forum Hub")
+                    .build();
+
+            decodedJWT = verifier.verify(token);
+            return decodedJWT.getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RegraDeNegocioException("Token inválido!");
+        }
     }
 
 }
--- src/main/java/br/com/forum_hub/infra/security/FiltroTokenAcesso.java
+++ src/main/java/br/com/forum_hub/infra/security/FiltroTokenAcesso.java
@@ -11,20 +11,29 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FiltroTokenAcesso extends OncePerRequestFilter{
+public class FiltroTokenAcesso extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        
-        //Recuperar o token da requisição
-        String token = null;
 
-        if(token != null){
-            //Validação do token
-        } 
+        // Recuperar o token da requisição
+        String token = recuperarTokenRequisicao(request);
+
+        if (token != null) {
+            // Validação do token
+            
+        }
 
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarTokenRequisicao(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader != null){
+            return authorizationHeader.replace("Bearer", "");
+        }
+        return null;
     }
 
 }
