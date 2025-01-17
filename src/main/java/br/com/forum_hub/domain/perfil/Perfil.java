d26a293e4c0e3f448323a33db2334cc4f08f8fe3
unknown <thiagobalves13@gmail.com>
2025-01-17 15:20:43 -0300

    feat(h2 BD)[Aula_05]: atualizando h2BD

--- src/main/java/br/com/forum_hub/domain/perfil/Perfil.java
+++ src/main/java/br/com/forum_hub/domain/perfil/Perfil.java
@@ -3,6 +3,8 @@ package br.com.forum_hub.domain.perfil;
 import org.springframework.security.core.GrantedAuthority;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,6 +20,7 @@ public class Perfil implements GrantedAuthority{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     private PerfilNome nome;
 
     @Override
--- src/main/java/br/com/forum_hub/domain/usuario/Usuario.java
+++ src/main/java/br/com/forum_hub/domain/usuario/Usuario.java
@@ -12,9 +12,13 @@ import br.com.forum_hub.domain.perfil.Perfil;
 import br.com.forum_hub.domain.perfil.PerfilNome;
 import br.com.forum_hub.infra.exception.RegraDeNegocioException;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -38,7 +42,9 @@ public class Usuario implements UserDetails {
     private String token;
     private LocalDateTime expiracaoToken;
     private Boolean ativo;
-    
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuarios_perfis", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "perfil_id"))
     private List<Perfil> perfis;
 
     @Override
